a32d800c05791e4f516e35e30ae48fdb57767782
gbharadhwaj4u@gmail.com <gbharadhwaj4u@gmail.com>
2017-12-17 21:22:03 +0530

    Finished http client exercise.

---
+++ learnoyounode/http-client.js
@@ -0,0 +1,11 @@
+let http = require('http');
+
+http.get(process.argv[2], function(response) {
+  response.setEncoding('utf8');
+  response.on('data', function(data){
+    console.log(data);
+  });
+  response.on('error', console.error);
+}).on('error', function(){
+  console.log(error);
+});
