facaf5f4d32a1553753b2a6c05e398600b6e6ddf
gbharadhwaj4u@gmail.com <gbharadhwaj4u@gmail.com>
2017-12-18 22:59:21 +0530

    Added READEME and renamed the folder name.

--- learnoyounode/http-file-server.js
+++
@@ -1,7 +0,0 @@
-let http = require('http');
-let fs = require('fs');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, { 'content-type': 'text/plain' });
-  fs.createReadStream(process.argv[3]).pipe(res);
-}).listen(process.argv[2]);
--- learnoyounode/my-first-io.js
+++
@@ -1,7 +0,0 @@
-// Load global fs module
-let fs = require('fs');
-let fileContent = fs.readFileSync(process.argv[2]).toString();
-// let fileContent = fs.readFileSync(process.argv[2], 'utf8'); // To get string
-let noOfNewLines = fileContent.split('\n').length;
-// Note that the test file does not have a newline character ('\n') at the end of the last line,
-console.log(noOfNewLines - 1);
