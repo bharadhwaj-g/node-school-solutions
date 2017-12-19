fc5cfa8dd2f0150d01ed8c1282914fb1cba2f9c9
gbharadhwaj4u@gmail.com <gbharadhwaj4u@gmail.com>
2017-12-20 00:52:50 +0530

    Finished Http-JSON challenge.

---
+++ learnyounode/http-json.js
@@ -0,0 +1,50 @@
+let http = require('http');
+let url = require('url');
+
+// Function to parse the request date.
+function getParseTime(date) {
+  let resObj = {
+    hour: new Date(date).getHours(),
+    minute: new Date(date).getMinutes(),
+    second: new Date(date).getSeconds()
+  };
+  return resObj;
+}
+
+// Function to get unixtime.
+function getUnixTime(date) {
+  let resObj = {
+    "unixtime": new Date(date).getTime()
+  };
+  return resObj;
+}
+
+// Create a http server
+http.createServer(function (req, res) {
+  if (req.method !== 'GET') {
+    return res.end('Only accepts GET method.')
+  }
+
+  // Read the request properties.
+  let reqProps = url.parse(req.url, true);
+  let reqDate = reqProps.query.iso ? reqProps.query.iso : '';
+  if (!reqDate) {
+    return res.end('Hey! do you forget to send iso param?')
+  }
+
+  let pathName = (reqProps.pathname) ? reqProps.pathname.toLowerCase() : '';
+  let response = '';
+  if (pathName === '/api/parsetime') {
+    response = getParseTime(reqDate);
+  } else if (pathName === '/api/unixtime') {
+    response = getUnixTime(reqDate);
+  } else {
+    res.writeHead(404);
+    res.end('Hey!, path not found, please send proper path.')
+  }
+
+  res.writeHead(200, {
+    'Content-Type': 'application/json'
+  });
+  res.end(JSON.stringify(response));
+}).listen(Number(process.argv[2]))
